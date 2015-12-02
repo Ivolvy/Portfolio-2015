1a48b823915f9829bdce991cbd3d6cdbd85f7f74
Ivolvy <michael.genty@free.fr>
2015-12-02 13:15:16 +0100

    Added mouse scroll button + change background colors

--- js/app.js
+++ js/app.js
@@ -58,3 +58,20 @@ $('.overlay-menu ul li a').on( 'click', function(event){
     flkty.select(parseInt(index));
 });
 
+
+//SMOOTH SCROLLING
+$(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html, body, #introduction-page').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});
+
